194775fb4f75334ca4904f1188dd184a67d480e8
Mahmoud Zakria <m.zakria90@gmail.com>
2024-06-14 02:47:50 +0200

    handle keyboard navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,24 @@ import modalCSS from "./App.module.css";
 // Component is inspired from HTML5 Dialog element
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog
 // This one even is going to make `React.portal()` obselete.
+// It supports `ESC` button for closing the dialog.
 function Modal() {
   const modalRef = useRef<HTMLDialogElement>(null);
   const showButtonRef = useRef<HTMLButtonElement>(null);
+  const saveButtonRef = useRef<HTMLButtonElement>(null);
   const closeButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     // No need to make an event delegation ;)
     const modal = modalRef.current;
     const showButton = showButtonRef.current;
+    const saveButton = saveButtonRef.current;
     const closeButton = closeButtonRef.current;
+
+    const handleClose = () => {
+      modal.close();
+      document.body.style.overflow = "visible";
+    };
 
     if (showButton && modal) {
       showButton.addEventListener("click", () => {
@@ -23,54 +31,87 @@ function Modal() {
       });
     }
 
-    if (closeButton && modal) {
-      closeButton.addEventListener("click", () => {
-        modal.close();
-        document.body.style.overflow = "visible";
+    if (saveButton && modal) {
+      saveButton.addEventListener("click", () => {
+        // Do Something
+        alert("Data Saved!");
+        handleClose();
       });
+    }
+
+    if (closeButton && modal) {
+      closeButton.addEventListener("click", handleClose);
     }
   }, []);
   return (
-    <section className={modalCSS.container}>
-      <dialog role="dialog" ref={modalRef} className={modalCSS.modal}>
-        <p tabIndex={0}>This modal dialog has a groovy backdrop!</p>
+    <>
+      <section className={modalCSS.container}>
+        <button
+          type="button"
+          ref={showButtonRef}
+          className={modalCSS.showButton}
+        >
+          Show modal
+        </button>
+
+        <article className={modalCSS.article}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+          repellendus tempore cupiditate possimus magnam pariatur consequuntur
+          amet ipsum ab repudiandae veniam fugit quidem temporibus qui iure est
+          nulla consequatur repellat? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Doloremque, repellendus tempore cupiditate possimus
+          magnam pariatur consequuntur amet ipsum ab repudiandae veniam fugit
+          quidem temporibus qui iure est nulla consequatur repellat? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Doloremque, repellendus
+          tempore cupiditate possimus magnam pariatur consequuntur amet ipsum ab
+          repudiandae veniam fugit quidem temporibus qui iure est nulla
+          consequatur repellat? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Doloremque, repellendus tempore cupiditate possimus
+          magnam pariatur consequuntur amet ipsum ab repudiandae veniam fugit
+          quidem temporibus qui iure est nulla consequatur repellat? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Doloremque, repellendus
+          tempore cupiditate possimus magnam pariatur consequuntur amet ipsum ab
+          repudiandae veniam fugit quidem temporibus qui iure est nulla
+          consequatur repellat? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Doloremque, repellendus tempore cupiditate possimus
+          magnam pariatur consequuntur amet ipsum ab repudiandae veniam fugit
+          quidem temporibus qui iure est nulla consequatur repellat? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Doloremque, repellendus
+          tempore cupiditate possimus magnam pariatur consequuntur amet ipsum ab
+          repudiandae veniam fugit quidem temporibus qui iure est nulla
+          consequatur repellat?
+        </article>
+      </section>
+      <dialog
+        role="dialog"
+        autoFocus
+        tabIndex={1}
+        ref={modalRef}
+        className={modalCSS.modal}
+      >
+        <header>
+          <h1 tabIndex={2}>Modal title</h1>
+        </header>
+        <section className={modalCSS.modalBody} tabIndex={3}>
+          This modal dialog has a groovy backdrop!This modal dialog has a groovy
+          backdrop!This modal dialog has a groovy backdrop!This modal dialog has
+          a groovy backdrop! This modal dialog has a groovy backdrop!This modal
+          dialog has a groovy backdrop!This modal dialog has a groovy
+          backdrop!This modal dialog has a groovy backdrop! This modal dialog
+          has a groovy backdrop!This modal dialog has a groovy backdrop!This
+          modal dialog has a groovy backdrop!This modal dialog has a groovy
+          backdrop! This modal dialog has a groovy backdrop!This modal dialog
+          has a groovy backdrop!This modal dialog has a groovy backdrop!This
+          modal dialog has a groovy backdrop!
+        </section>
+        <button type="button" ref={saveButtonRef}>
+          Save
+        </button>
         <button type="button" ref={closeButtonRef}>
           Close
         </button>
       </dialog>
-
-      <button type="button" ref={showButtonRef} className={modalCSS.showButton}>
-        Show the dialog
-      </button>
-
-      <article className={modalCSS.article}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-        repellendus tempore cupiditate possimus magnam pariatur consequuntur
-        amet ipsum ab repudiandae veniam fugit quidem temporibus qui iure est
-        nulla consequatur repellat? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Doloremque, repellendus tempore cupiditate possimus
-        magnam pariatur consequuntur amet ipsum ab repudiandae veniam fugit
-        quidem temporibus qui iure est nulla consequatur repellat? Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Doloremque, repellendus
-        tempore cupiditate possimus magnam pariatur consequuntur amet ipsum ab
-        repudiandae veniam fugit quidem temporibus qui iure est nulla
-        consequatur repellat? Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Doloremque, repellendus tempore cupiditate possimus magnam
-        pariatur consequuntur amet ipsum ab repudiandae veniam fugit quidem
-        temporibus qui iure est nulla consequatur repellat? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Doloremque, repellendus tempore
-        cupiditate possimus magnam pariatur consequuntur amet ipsum ab
-        repudiandae veniam fugit quidem temporibus qui iure est nulla
-        consequatur repellat? Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Doloremque, repellendus tempore cupiditate possimus magnam
-        pariatur consequuntur amet ipsum ab repudiandae veniam fugit quidem
-        temporibus qui iure est nulla consequatur repellat? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Doloremque, repellendus tempore
-        cupiditate possimus magnam pariatur consequuntur amet ipsum ab
-        repudiandae veniam fugit quidem temporibus qui iure est nulla
-        consequatur repellat?
-      </article>
-    </section>
+    </>
   );
 }
 
